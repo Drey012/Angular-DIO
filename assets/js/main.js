b0bf5f8c2f98c0a2cea2d75f2c3fe5ecc52813f9
drey012 <cleytonteamoo@gmail.com>
2024-10-30 16:42:57 -0300

    em desenvolvimento

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,64 @@
-console.log('Conectado');
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+// define o número máximo de pokemons que podem ser carregados
+const maxRecords = 151
+// define quantos pokemons devem ser carregados por vez
+const limit = 10
+// define qual o offset inicial, ou seja, qual o primeiro pokemon que deve ser carregado
+let offset = 0;
+
+// essa função é responsável por converter um pokemon em um elemento <li> que pode ser adicionado na lista
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="ordem-pk ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+// essa função é responsável por carregar os pokemons com base no offset e limit
+function loadPokemonItens(offset, limit) {
+    // chama a função getPokemons do arquivo poke-api.js e passa o offset e limit como parâmetro
+    pokedex.getPokemons(offset, limit).then((pokemons = []) => {
+        // converte os pokemons em elementos <li>
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        // adiciona os elementos <li> na lista de pokemons
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+// carrega os primeiros pokemons com base no offset e limit
+loadPokemonItens(offset, limit)
+
+// adiciona um evento de click no botão para carregar mais pokemons
+loadMoreButton.addEventListener('click', () => {
+    // incrementa o offset para carregar os próximos pokemons
+    offset += limit
+    // calcula quantos pokemons devem ser carregados na próxima página
+    const qtdRecordsWithNexPage = offset + limit
+
+    // se o número de pokemons que devem ser carregados for maior que o número máximo de pokemons
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        // calcula quantos pokemons devem ser carregados para completar a lista
+        const newLimit = maxRecords - offset
+        // carrega os pokemons com o novo limit
+        loadPokemonItens(offset, newLimit)
+
+        // remove o botão para carregar mais pokemons pois não há mais pokemons para carregar
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        // carrega os próximos pokemons com base no offset e limit
+        loadPokemonItens(offset, limit)
+    }
+})  
---
+++ assets/js/poke-api.js
@@ -0,0 +1,39 @@
+const pokedex = {};
+
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name )
+    const [type] = types;
+    pokemon.types = types;
+    pokemon.type = type;
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    return pokemon
+}
+
+pokedex.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon)
+}
+
+
+
+pokedex.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    // requisição no servidor da API
+    return fetch(url)
+    .then((response) => response.json())
+    //exibe a lista de pokemons do objeto json
+    .then((jsonBody) => jsonBody.results)
+    // converte os pokemons em elementos <li>
+    .then((pokemons) => pokemons.map(pokedex.getPokemonDetail))
+    // pega os detalhes dos pokemons
+    .then((detailRequests) => Promise.all(detailRequests))
+    // converte os detalhes dos pokemons em objetos
+    .then((pokemonsDetails) => pokemonsDetails)
+}
